108659e240c90ca80a6689fa9dc6051e45403d11
warrendk <u1029004@utah.edu>
2022-05-04 17:04:20 -0600

    migration issues on heroku, removed some migration files

--- db/migrate/20220503224218_drop_stats_table.rb
+++
@@ -1,9 +0,0 @@
-class DropStatsTable < ActiveRecord::Migration[7.0]
-  def up
-    drop_table :stats
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
--- db/migrate/20220503231258_drop_stats_again.rb
+++
@@ -1,9 +0,0 @@
-class DropStatsAgain < ActiveRecord::Migration[7.0]
-   def up
-    drop_table :stats
-  end
-
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
-end
